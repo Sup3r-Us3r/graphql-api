7579b7ac264bd068c71abbc0815ae5ce23107ea6
Mayderson Mello <maydersonmello@gmail.com>
2024-09-16 17:05:51 -0300

    create new fields in todo dto

--- src/todo/dto/create-todo.input.ts
+++ src/todo/dto/create-todo.input.ts
@@ -1,7 +1,53 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, Int, Float } from '@nestjs/graphql';
+import {
+  IsOptional,
+  IsString,
+  IsBoolean,
+  IsArray,
+  IsEnum,
+  IsInt,
+  IsNumber,
+} from 'class-validator';
+
+import { TodoStatus } from '../entities/todo.entity';
 
 @InputType()
 export class CreateTodoInput {
   @Field()
+  @IsString()
   title: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @Field(() => TodoStatus, { defaultValue: TodoStatus.PENDING })
+  @IsEnum(TodoStatus)
+  status: TodoStatus;
+
+  @Field(() => [String], { nullable: true })
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  tags?: string[];
+
+  @Field(() => Float, { nullable: true })
+  @IsOptional()
+  @IsNumber()
+  priority?: number;
+
+  @Field(() => Boolean, { defaultValue: false })
+  @IsBoolean()
+  isUrgent: boolean;
+
+  @Field(() => Int, { nullable: true })
+  @IsOptional()
+  @IsInt()
+  assignedToId?: number;
+
+  @Field(() => Int, { nullable: true })
+  @IsOptional()
+  @IsInt()
+  parentTaskId?: number;
 }
